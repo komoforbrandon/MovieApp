78fd8590b8e0bb2201ea9adcda8669789465d9cb
KBrand <kbrandn25@gmail.com>
2026-04-23 13:12:00 +0100

    the movie app file structure created and structured to a required react file structured

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <>
+      <h1>Welcome to React with TypeScript!</h1>
+      <p>This is a simple React application using TypeScript.</p>
+    </>
+  )
+}
+
+export default App
